3273327ffa5ed92f438bc93b01fa451e7a1ca869
kou12345 <hi.0412.ko.2002@gmail.com>
2024-07-28 18:44:30 +0900

    add

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-import pymupdf
+import pymupdf4llm
+import pathlib
 
-PDF_PATH = "/Users/kou12345/Downloads/コンピュータアーキテクチャのエッセンス 第2版.pdf"
+
+PDF_PATH = ""
 
 # テキストを取得する
-doc = pymupdf.open(PDF_PATH)  # open a document
-out = open("output.txt", "wb")  # create a text output
-for page in doc:  # iterate the document pages
-    text = page.get_text().encode("utf8")  # get plain text (is in UTF-8)
-    out.write(text)  # write text of page
-    out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
-out.close()
+# doc = pymupdf.open(PDF_PATH)  # open a document
+# out = open("output.txt", "wb")  # create a text output
+# for page in doc:  # iterate the document pages
+#     text = page.get_text().encode("utf8")  # get plain text (is in UTF-8)
+#     out.write(text)  # write text of page
+#     out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
+# out.close()
+
+
+md_text = pymupdf4llm.to_markdown(PDF_PATH)
+
+# now work with the markdown text, e.g. store as a UTF8-encoded file
+
+pathlib.Path("output.md").write_bytes(md_text.encode())
